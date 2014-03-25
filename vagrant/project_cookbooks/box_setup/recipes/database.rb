c0b350145374c1b7e3972a24a192caf5113bfe56
Jonathan Hartman <jhartman86@gmail.com>
2014-03-25 00:07:04 -0600

    Auto-format all ruby code for better readability

--- vagrant/project_cookbooks/box_setup/recipes/database.rb
+++ vagrant/project_cookbooks/box_setup/recipes/database.rb
@@ -2,9 +2,9 @@
 # will fail silently
 mysql_database node[:box_setup][:concrete5][:db_name] do
   connection(
-    :host     => node[:box_setup][:concrete5][:db_server],
-    :username => node[:box_setup][:concrete5][:db_username],
-    :password => node[:mysql][:server_root_password]
+      :host => node[:box_setup][:concrete5][:db_server],
+      :username => node[:box_setup][:concrete5][:db_username],
+      :password => node[:mysql][:server_root_password]
   )
   action :create
 end
